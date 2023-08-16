c85e141cfda23b05e275ae21e3c6e7819e32df76
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-08-17 00:31:05 +0400

    first commit

---
+++ src/main/java/ru/devbulygin/SpringTest2Bot/SpringTest2BotApplication.java
@@ -0,0 +1,13 @@
+package ru.devbulygin.SpringTest2Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest2BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest2BotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/devbulygin/SpringTest2Bot/SpringTest2BotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.devbulygin.SpringTest2Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest2BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
